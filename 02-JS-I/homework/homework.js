ad4108aadffe2d4f7a9f406e866a7f42a0921abb
hernansoliman2021 <81513358+hernansoliman2021@users.noreply.github.com>
2021-03-29 23:30:33 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -141,21 +141,25 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  return  (Math.pow (num, exponent));
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  return  (Math.round());
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return  (Math.ceil());
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+  return  (Math.random());
 }
 
 function esPositivo(numero) {
@@ -163,6 +167,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if ( numero > 0)  { return ( "Es positivo" ) }
+  if ( numero < 0)  { return ( "Es negativo" ) }
+  if ( numero === 0){ return ( false ) }
 }
 
 function agregarSimboloExclamacion(str) {
