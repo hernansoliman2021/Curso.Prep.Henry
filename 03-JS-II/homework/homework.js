a8d675a84b36ac615f0e4fb476071bd7517de6df
hernansoliman2021 <81513358+hernansoliman2021@users.noreply.github.com>
2021-04-01 14:51:23 -0300

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -49,6 +49,20 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if ( idioma === "aleman" ){
+    return ( " Guten Tag !" );
+  }
+  if ( idioma === "mandarin" ){
+    return ( " Ni Hao! " )
+  }
+  if ( idioma === "ingles" ){
+    return (" Hello!" );
+  }
+  if ( idioma === 'undefined' ){
+    return (" Hola!" );
+  }else {
+     return (" Hola!" );
+  }
 }
 
 function colors(color) {
@@ -59,12 +73,29 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+   switch(color) {
+    case "blue":
+      return "This is blue";
+    case "red": 
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default: 
+    return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( numero === 10 || numero === 5 ){
+    return  ( true );
+  }else {
+    return  ( false );
+  }
 }
 
 function estaEnRango(numero) {
